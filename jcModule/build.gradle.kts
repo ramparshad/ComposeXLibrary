83098411011578333bd85b05bbed72a1de37fda6
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-08-30 00:29:52 +0530

    Update build.gradle.kts

--- jcModule/build.gradle.kts
+++ jcModule/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
     id("maven-publish") 
 
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" // ✅ REQUIRED
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" // ✅ 
 
 }
 
